041b6f33a2f5c9d5481ddf9d5c53ac917b4ded1f
Abdelrahman Hesham <abdelrhmanh.elzaher@gmail.com>
2022-06-02 23:15:54 +0200

    Add my first Exercise in First Assigment

---
+++ Assigment1_Ex1_Main.c
@@ -0,0 +1,23 @@
+/*
+ * Main.c
+ *
+ *  Created on: 2 Jun 2022
+ *      Author: SEE TECH
+ */
+#include "stdio.h"
+/*
+EX1:
+
+Write C Program to Print a Sentence
+
+i should see the Console as following:
+##########Console-output###
+C Programming
+
+*/
+int main()
+{
+	printf("##########Console-output###\nC Programming###########################\n\n"
+			"########################################################################");
+}
+
