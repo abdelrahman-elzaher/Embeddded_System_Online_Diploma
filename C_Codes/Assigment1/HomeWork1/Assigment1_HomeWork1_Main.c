3b967bfc6764fb0f81300849c2ce9b83396b3f49
Abdelrahman Hesham <abdelrhmanh.elzaher@gmail.com>
2022-06-02 23:42:14 +0200

    Creating the Folders for C programms and Add First Project

---
+++ C_Codes/Assigment1/HomeWork1/Assigment1_HomeWork1_Main.c
@@ -0,0 +1,21 @@
+/*
+ 
+   Created on: 2 Jun 2022
+       Author: SEE TECH
+ 
+	EX1:
+
+	Write C Program to Print a Sentence
+
+	i should see the Console as following:
+	##########Console-output###
+	C Programming
+
+ */
+#include "stdio.h"
+
+int main()
+{
+	printf("##########Console-output###\nC Programming###########################\n\n"
+			"########################################################################");
+}
